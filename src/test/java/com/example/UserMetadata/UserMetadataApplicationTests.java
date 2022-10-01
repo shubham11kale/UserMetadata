90c80c9d74faa2f4be378f471e791913e8f68e02
Shubham Kale (Onward) <Shubham.Kale@piramal.com>
2022-10-01 13:02:25 +0530

    Initial commit

---
+++ src/test/java/com/example/UserMetadata/UserMetadataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UserMetadata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMetadataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
